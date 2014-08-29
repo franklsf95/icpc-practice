47b303db647b3194b1fa67c38b38d515fa010c26
Frank Luan <franklsf95@gmail.com>
2014-08-28 18:23:19 -0700

    Solved 2013-PacNW/Escape (BFS + PriorityQueue)

---
+++ 2013-PacNW/src/DeltaQuadrant.java
@@ -0,0 +1,26 @@
+/**
+ * @author luans
+ * @created Aug 28, 2014
+ */
+import java.util.Scanner;
+
+public class DeltaQuadrant {
+	
+	static int N, k;
+	
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int nCases = in.nextInt();
+		for (int i = 1; i <= nCases; i++) {
+			N = in.nextInt();
+			k = in.nextInt();
+			for (int j = 0; j < N - 1; j++) {
+				int u = in.nextInt();
+				int v = in.nextInt();
+				int t = in.nextInt();
+			}
+		}
+		
+	}
+}
--- 2013-PacNW/src/Escape.java
+++ 2013-PacNW/src/Escape.java
@@ -1,3 +1,11 @@
+/**
+ * @author luans
+ * @created Aug 28, 2014
+ */
+
+// Breadth-first Search using Priority Queue
+// A hash set or a boolean array must be used for re-visit checking
+
 import java.util.*;
 
 public class Escape {
@@ -5,14 +13,20 @@ public class Escape {
 	static class Point {
 		int x;
 		int y;
+		int time; // time after passing this point
 		
-		Point() {
-			x = y = 0;
-		}
-		
-		Point(int _x, int _y) {
+		Point(int _y, int _x, int[][] map, int base) {
 			x = _x;
 			y = _y;
+			time = map[y][x] + base;
+		}
+		
+		boolean win(int w, int h) {
+			if (x == 0 || x == w - 1 || y == 0 || y == h - 1) {
+//				System.out.println(this + " wins! " + w + " " + h);
+				return true;
+			}
+			return false;
 		}
 		
 		public boolean equals(Object o) {
@@ -20,12 +34,34 @@ public class Escape {
 			return x == q.x && y == q.y;
 		}
 		
+		public int hashCode() {
+			return x * 1009 + y;
+		}
+		
 		public String toString() {
-			return "(" + x + ", " + y + ")";
+			return "(" + x + "," + y + ")[" + time + "]";
 		}
 	}
 	
-
+	static class MyQueue extends PriorityQueue<Point> {
+		HashSet<Point> visited;
+		
+		MyQueue(int initialCapacity, Comparator<Point> comparator) {
+			super(initialCapacity, comparator);
+			visited = new HashSet<Point>();
+		}
+		
+		public boolean add(Point p) {
+			if (visited.contains(p)) {
+//				System.out.println("Duplicate " + p);
+				return false;
+			}
+//			System.out.println("Pushed " + p);
+			visited.add(p);
+			return super.add(p);
+		}
+	}
+	
 	static void printMap(int[][] map, int w, int h) {
 		for (int i = 0; i < h; i++) {
 			for (int j = 0; j < w; j++) {
@@ -35,30 +71,56 @@ public class Escape {
 		}
 	}
 	
-	static int enqueue(ArrayList<Point> q, int now, int x, int y) {
-		Point p = new Point(x, y);
-		System.out.println("Trying to enqueue " + p + "; now is " + now);
-		for (int i = 0; i < now; i++) {
-			if (q.get(i).equals(p)) {
-				System.out.println("We already processed " + p + " at " + i);
-				return now;
+	static int solve(Point start, int[][] map, int w, int h) {
+		MyQueue queue = new MyQueue(100, new Comparator<Point>() {
+	        public int compare(Point p, Point q) {
+	        	int ret = p.time - q.time;
+//	        	System.out.println(p + " - " + q + " = " + ret);
+	        	return ret;
+	        }
+	    });
+		
+		queue.add(start);
+		
+		while (!queue.isEmpty()) {
+			Point p = queue.remove();
+			if (p.win(w, h)) {
+				return p.time;
+			}
+//			System.out.println("Processing " + p);
+			// generate new Points
+			Point q;
+			if (p.x > 0) { // left
+				q = new Point(p.y, p.x - 1, map, p.time);
+				queue.add(q);
+			}
+			if (p.x < w - 1) { // right
+				q = new Point(p.y, p.x + 1, map, p.time);
+				queue.add(q);
+			}
+			if (p.y > 0) { // up
+				q = new Point(p.y - 1, p.x, map, p.time);
+				queue.add(q);
+			}
+			if (p.y < h - 1) { //down
+				q = new Point(p.y + 1, p.x, map, p.time);
+				queue.add(q);
 			}
 		}
-		q.add(p);
-		return now + 1;
+		return -1; // should never happen
 	}
 	
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+//		System.out.println("Debug");
 		
-		int TT = in.nextInt();
-		for (int i = 0; i < TT; i++) {
-			int k = in.nextInt();
+		Scanner in = new Scanner(System.in);
+		int nCases = in.nextInt();
+		for (int i = 1; i <= nCases; i++) {
+			int K = in.nextInt();
 			int w = in.nextInt();
 			int h = in.nextInt();
-			
 			HashMap<String, Integer> dic = new HashMap<String, Integer>();
-			for (int j = 0; j < k; j++) {
+			for (int j = 0; j < K; j++) {
 				String cls = in.next();
 				int duration = in.nextInt();
 				dic.put(cls, duration);
@@ -67,52 +129,20 @@ public class Escape {
 			dic.put("E", 0);
 			
 			int[][] T = new int[h][w];
-			int[][] f = new int[h][w];
-			Point ent = null;
+			Point start = null;
 			
 			for (int u = 0; u < h; u++) {
 				String buf = in.nextLine();
 				for (int v = 0; v < w; v++) {
 					T[u][v] = dic.get(buf.substring(v, v + 1));
 					if (T[u][v] == 0) {
-						ent = new Point(u, v);
+						start = new Point(u, v, T, 0);
 					}
-					f[u][v] = 2147483647;
 				}
 			}
-			
-			printMap(T, w, h);
-			
-			int now = 0;
-			ArrayList<Point> queue = new ArrayList<Point>();
-			f[ent.x][ent.y] = 0;
-			queue.add(ent);
-			while (now != queue.size()) {
-				Point cur = queue.get(now);
-				now++;
-				System.out.println("Current point is " + cur);
-				
-				if (cur.y > 0 && f[cur.x][cur.y] < f[cur.x][cur.y - 1] + T[cur.x][cur.y]) {
-					f[cur.x][cur.y] = f[cur.x][cur.y - 1] + T[cur.x][cur.y];
-					now = enqueue(queue, now, cur.x, cur.y - 1);
-				}
-				if (cur.y < h && f[cur.x][cur.y] < f[cur.x][cur.y + 1] + T[cur.x][cur.y]) {
-					f[cur.x][cur.y] = f[cur.x][cur.y + 1] + T[cur.x][cur.y];
-					now = enqueue(queue, now, cur.x, cur.y + 1);
-				}
-				if (cur.x > 0 && f[cur.x][cur.y] < f[cur.x - 1][cur.y] + T[cur.x][cur.y]) {
-					f[cur.x][cur.y] = f[cur.x - 1][cur.y] + T[cur.x][cur.y];
-					now = enqueue(queue, now, cur.x - 1, cur.y);
-				}
-				if (cur.x < w && f[cur.x][cur.y] < f[cur.x + 1][cur.y] + T[cur.x][cur.y]) {
-					f[cur.x][cur.y] = f[cur.x + 1][cur.y] + T[cur.x][cur.y];
-					now = enqueue(queue, now, cur.x + 1, cur.y);
-				}
-			}
-			
-			printMap(f, w, h);
+			int result = solve(start, T, w, h);
+			System.out.println(result);
 		}
-
+		
 	}
-
 }
