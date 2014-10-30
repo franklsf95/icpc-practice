29f0a373038a6b0afe3b2975f7cbfa4c49730e77
Frank LSF <franklsf95@gmail.com>
2014-10-30 17:55:44 -0500

    Set up 2012-MidCentral

---
+++ 2012-MidCentral/judge.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Automated Judging system
+
+echo $1
+if [ "$1" == "" ]
+then
+    echo 'Need to indicate source class name'
+    exit
+fi
+
+SRC="$1"
+src=$(echo $SRC | tr '[:upper:]' '[:lower:]')
+
+mkdir -p output
+javac -encoding UTF-8 -sourcepath . -d . src/$SRC.java
+
+echo 'Program starts...'
+time java -client -Xss8m -Xmx2048m $SRC <problems/$src/in.txt >output/$src.out
+
+echo '[Empty means AC, left (<) is program output, right (>) is standard output]'
+diff output/$src.out problems/$src/out.txt --strip-trailing-cr | more
+
+rm *.class
---
+++ 2012-MidCentral/src/Revenge.java
@@ -0,0 +1,5 @@
+public class Revenge {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
